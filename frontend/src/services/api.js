69f904839585b29f040094eebd68864f1a62f1c6
shraddha <shraddhashirooru5@gmail.com>
2026-04-25 19:43:56 +0530

    fix routing for deploy

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
-  timeout: 5000,
+  baseURL: process.env.REACT_APP_API_URL || "https://vehicle-service-backend-pflq.onrender.com/api",
 });
 
 // REQUEST LOGGER
